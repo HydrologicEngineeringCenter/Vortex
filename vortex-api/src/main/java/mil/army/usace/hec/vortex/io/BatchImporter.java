22011a636c28437065e4286e6ea5d2f1db34f1af
Thomas Brauer <thomas.brauer@usace.army.mil>
2019-09-01 13:04:19 -0700

    Refactor MetDataImport to BatchImporter

--- vortex-api/src/main/java/mil/army/usace/hec/vortex/io/BatchImporter.java
+++ vortex-api/src/main/java/mil/army/usace/hec/vortex/io/BatchImporter.java
@@ -6,14 +6,14 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MetDataImport {
+public class BatchImporter {
     private List<Path> inFiles;
     private List<String> variables;
     private Path destination;
     private Options geoOptions;
     private Options writeOptions;
 
-    private MetDataImport(MetDataImportBuilder builder){
+    private BatchImporter(BatchImporterBuilder builder){
         this.inFiles = builder.inFiles;
         this.variables = builder.variables;
         this.destination = builder.destination;
@@ -21,44 +21,44 @@ public class MetDataImport {
         this.writeOptions = builder.writeOptions;
     }
 
-    public static class MetDataImportBuilder{
+    public static class BatchImporterBuilder {
         private List<Path> inFiles;
         private List<String> variables;
         private Path destination;
         private Options geoOptions;
         private Options writeOptions;
 
-        public MetDataImportBuilder inFiles(final List<Path> inFiles){
+        public BatchImporterBuilder inFiles(final List<Path> inFiles){
             this.inFiles = inFiles;
             return this;
         }
 
-        public MetDataImportBuilder variables(final List<String> variables){
+        public BatchImporterBuilder variables(final List<String> variables){
             this.variables = variables;
             return this;
         }
 
-        public MetDataImportBuilder destination(final Path destination){
+        public BatchImporterBuilder destination(final Path destination){
             this.destination = destination;
             return this;
         }
 
-        public MetDataImportBuilder geoOptions(final Options geoOptions){
+        public BatchImporterBuilder geoOptions(final Options geoOptions){
             this.geoOptions = geoOptions;
             return this;
         }
 
-        public MetDataImportBuilder writeOptions(final Options writeOptions){
+        public BatchImporterBuilder writeOptions(final Options writeOptions){
             this.writeOptions = writeOptions;
             return this;
         }
 
-        public MetDataImport build(){
-            return new MetDataImport(this);
+        public BatchImporter build(){
+            return new BatchImporter(this);
         }
     }
 
-    public static MetDataImportBuilder builder() {return new MetDataImportBuilder();}
+    public static BatchImporterBuilder builder() {return new BatchImporterBuilder();}
 
     public void process() {
 
