ad6669b81aa0f933afb8dab953b62fd4e41ebda8
Thomas Brauer <thomas.brauer@usace.army.mil>
2019-09-04 16:04:22 -0700

    Add isShg method test

---
+++ vortex-api/src/test/java/mil/army/usace/hec/vortex/geo/ReferenceUtilsTest.java
@@ -0,0 +1,28 @@
+package mil.army.usace.hec.vortex.geo;
+
+import hec.heclib.grid.AlbersInfo;
+import hec.heclib.grid.GridInfo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ReferenceUtilsTest {
+
+    @Test
+    void IsShgReturnsTrueForShgWithProjectionUnitsM(){
+        AlbersInfo info = mock(AlbersInfo.class);
+        when(info.getGridType()).thenReturn(420);
+        when(info.getFirstStandardParallel()).thenReturn((float) 29.5);
+        when(info.getSecondStandardParallel()).thenReturn((float) 45.5);
+        when(info.getLatitudeOfProjectionOrigin()).thenReturn((float) 23.0);
+        when(info.getCentralMeridian()).thenReturn((float) -96.0);
+        when(info.getFalseEasting()).thenReturn((float) 0);
+        when(info.getFalseNorthing()).thenReturn((float) 0);
+        when(info.getProjectionDatum()).thenReturn(GridInfo.getNad83());
+        when(info.getProjectionUnits()).thenReturn("m");
+        assertTrue(ReferenceUtils.isShg(info));
+    }
+
+}
