89ac484fd34612558c53e055ff5058d9c05e2dfe
Thomas Brauer <thomas.brauer@usace.army.mil>
2019-09-05 13:28:55 -0700

    Add some additional tests for isShg

--- vortex-api/src/test/java/mil/army/usace/hec/vortex/geo/ReferenceUtilsTest.java
+++ vortex-api/src/test/java/mil/army/usace/hec/vortex/geo/ReferenceUtilsTest.java
@@ -4,6 +4,7 @@ import hec.heclib.grid.AlbersInfo;
 import hec.heclib.grid.GridInfo;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,6 +24,36 @@ class ReferenceUtilsTest {
         when(info.getProjectionDatum()).thenReturn(GridInfo.getNad83());
         when(info.getProjectionUnits()).thenReturn("m");
         assertTrue(ReferenceUtils.isShg(info));
+    }
+
+    @Test
+    void IsShgReturnsTrueForShgWithProjectionUnitsMeter(){
+        AlbersInfo info = mock(AlbersInfo.class);
+        when(info.getGridType()).thenReturn(420);
+        when(info.getFirstStandardParallel()).thenReturn((float) 29.5);
+        when(info.getSecondStandardParallel()).thenReturn((float) 45.5);
+        when(info.getLatitudeOfProjectionOrigin()).thenReturn((float) 23.0);
+        when(info.getCentralMeridian()).thenReturn((float) -96.0);
+        when(info.getFalseEasting()).thenReturn((float) 0);
+        when(info.getFalseNorthing()).thenReturn((float) 0);
+        when(info.getProjectionDatum()).thenReturn(GridInfo.getNad83());
+        when(info.getProjectionUnits()).thenReturn("meter");
+        assertTrue(ReferenceUtils.isShg(info));
+    }
+
+    @Test
+    void IsShgReturnsFalseForShgWithProjectionUnitsFeet(){
+        AlbersInfo info = mock(AlbersInfo.class);
+        when(info.getGridType()).thenReturn(420);
+        when(info.getFirstStandardParallel()).thenReturn((float) 29.5);
+        when(info.getSecondStandardParallel()).thenReturn((float) 45.5);
+        when(info.getLatitudeOfProjectionOrigin()).thenReturn((float) 23.0);
+        when(info.getCentralMeridian()).thenReturn((float) -96.0);
+        when(info.getFalseEasting()).thenReturn((float) 0);
+        when(info.getFalseNorthing()).thenReturn((float) 0);
+        when(info.getProjectionDatum()).thenReturn(GridInfo.getNad83());
+        when(info.getProjectionUnits()).thenReturn("ft");
+        assertFalse(ReferenceUtils.isShg(info));
     }
 
 }
