518ebe6685141f590c844e0a9870ed0540644178
Thomas Brauer <thomas.brauer@usace.army.mil>
2019-09-07 15:33:01 -0700

    Intermediate check-in. Independent directory controller and file controller.

---
+++ image-exporter/src/main/java/exporter/ExporterWizard.java
@@ -0,0 +1,48 @@
+package exporter;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import mil.army.usace.hec.vortex.GdalRegister;
+
+public class ExporterWizard extends Application {
+
+    static {
+        GdalRegister.getInstance();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        final Injector injector = Guice.createInjector( new WizardModule() );
+
+        final Parent p = FXMLLoader.load( ExporterWizard.class.getResource("/fxml/Wizard.fxml"),
+                null,
+                new JavaFXBuilderFactory(),
+                injector::getInstance
+        );
+
+        final Scene scene = new Scene(p);
+
+        primaryStage.setScene( scene );
+        primaryStage.setWidth( 600 );
+        primaryStage.setHeight( 400 );
+        primaryStage.setTitle("Image Exporter Wizard");
+        primaryStage.getIcons().add(new Image("/vortex_black.png"));
+        primaryStage.initStyle(StageStyle.DECORATED);
+
+        ViewSwitcher switcher = new ViewSwitcher();
+        switcher.switchView(primaryStage);
+    }
+
+    public static void main(String[] args) { launch(args); }
+
+}
+
---
+++ image-exporter/src/main/java/exporter/controller/Validate.java
@@ -0,0 +1,11 @@
+package exporter.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Validate {
+}
