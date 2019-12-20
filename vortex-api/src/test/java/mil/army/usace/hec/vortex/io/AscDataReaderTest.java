3fe6f66b60dd1665f04755b193ee3c7383e8cc1f
Thomas Brauer <thomas.brauer@usace.army.mil>
2019-12-20 05:19:40 -0800

    Add support for QPF ASC file.

--- vortex-api/src/test/java/mil/army/usace/hec/vortex/io/AscDataReaderTest.java
+++ vortex-api/src/test/java/mil/army/usace/hec/vortex/io/AscDataReaderTest.java
@@ -4,46 +4,47 @@ import mil.army.usace.hec.vortex.VortexGrid;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AscDataReaderTest {
 
-    private static VortexGrid grid;
+    @Test
+    void PrismImportPassesRegression() {
 
-    static {
-        String path = new File(AscDataReaderTest.class.getResource(
+        String path = new File(getClass().getResource(
                 "/regression/io/asc_reader/PRISM_ppt_stable_4kmD2_20170101_asc.asc").getFile()).toString();
 
         DataReader reader = DataReader.builder()
                 .path(path)
+                .variable("ppt")
                 .build();
 
-        grid = (VortexGrid) reader.getDtos().get(0);
-    }
+        VortexGrid grid = (VortexGrid) reader.getDtos().get(0);
 
-    @Test
-    void PrismPrecipImportMaxValuePassesRegression() {
         float[] data = grid.data();
         float max = Float.MIN_VALUE;
         for (float datum : data) {
             max = Math.max(max, datum);
         }
         assertEquals(208.298, max, 1E-3);
-    }
-
-    @Test
-    void PrismPrecipImportDxPassesRegression() {
         assertEquals(.04167, grid.dx(), 1E-3);
-    }
-
-    @Test
-    void PrismPrecipImportNyPassesRegression() {
         assertEquals(621, grid.ny());
+        assertEquals(1405, grid.nx());
     }
 
     @Test
-    void PrismPrecipImportNxPassesRegression() {
-        assertEquals(1405, grid.nx());
+    void QpfHourlyImport(){
+        String path = new File(getClass().getResource("/QPF_1HR_AZ_19121900.asc").getFile()).toString();
+
+        DataReader reader = DataReader.builder()
+                .path(path)
+                .variable("precipitation")
+                .build();
+
+        VortexGrid grid = (VortexGrid) reader.getDtos().get(0);
+        assertEquals(grid.endTime(), ZonedDateTime.of(2019, 12,19, 0, 0, 0, 0, ZoneId.of("UTC")));
     }
 }
