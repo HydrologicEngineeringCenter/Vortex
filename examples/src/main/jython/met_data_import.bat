343576d43c66e3005cdbb2dea591f7d53b4ce676
Thomas Brauer <thomas.brauer@usace.army.mil>
2021-02-12 08:46:11 -0800

    Update sample jython bat scripts to reflect latest Vortex build.

--- examples/src/main/jython/met_data_import.bat
+++ examples/src/main/jython/met_data_import.bat
@@ -1,7 +1,6 @@
-set "VORTEX_HOME=C:\Programs\vortex-0.8.7"
+set "VORTEX_HOME=C:\Programs\vortex-0.10.16"
 set "PATH=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal;%PATH%"
-set "GDAL_DRIVER_PATH=%VORTEX_HOME%\bin\gdal\gdalplugins"
 set "GDAL_DATA=%VORTEX_HOME%\bin\gdal\gdal-data"
 set "PROJ_LIB=%VORTEX_HOME%\bin\gdal\projlib"
 set "CLASSPATH=%VORTEX_HOME%\lib\*"
-C:\jython2.7.1\bin\jython.exe -J-Xmx2g -Djava.library.path=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal met_data_import.py
+C:\Programs\jython2.7.2\bin\jython.exe -J-Xmx2g -Djava.library.path=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal met_data_import.py
--- examples/src/main/jython/normalize.bat
+++ examples/src/main/jython/normalize.bat
@@ -1,6 +1,5 @@
-set "VORTEX_HOME=C:\Programs\vortex-0.10.11"
+set "VORTEX_HOME=C:\Programs\vortex-0.10.16"
 set "PATH=%VORTEX_HOME%\bin;%VORTEX_HOME%\bin\gdal;%PATH%"
-set "GDAL_DRIVER_PATH=%VORTEX_HOME%\bin\gdal\gdalplugins"
 set "GDAL_DATA=%VORTEX_HOME%\bin\gdal\gdal-data"
 set "PROJ_LIB=%VORTEX_HOME%\bin\gdal\projlib"
 set "CLASSPATH=%VORTEX_HOME%\lib\*"
