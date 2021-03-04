13ae659d41cc7c20ce1d37e5bba628c9afd0fde3
Thomas Brauer <thomas.brauer@usace.army.mil>
2021-03-03 21:34:59 -0800

    Remove GDAL_DRIVER_PATH environment variable from batch launch scripts.

--- importer/package/windows/importer.bat
+++ importer/package/windows/importer.bat
@@ -1,5 +1,4 @@
 SET "PATH=.\gdal;%PATH%"
-SET "GDAL_DRIVER_PATH=.\gdal\gdalplugins"
 SET "GDAL_DATA=.\gdal\gdal-data"
 SET "PROJ_LIB=.\gdal\projlib"
 "..\jre\bin\java.exe" --module-path "..\jmods" --add-modules javafx.controls,javafx.fxml -Djavafx.cachedir=. -Djava.library.path=".;.\gdal" -cp ..\lib\importer.jar;..\lib\* importer.MetDataImportWizard
