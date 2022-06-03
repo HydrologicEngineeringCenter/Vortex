4ed7e5585068ac3ed3e85cf2b1ee8593918267c0
Tom Brauer <thomas.brauer@usace.army.mil>
2022-06-03 10:26:30 -0700

    Add data type test for UA SWE data in time-shifter utility.

--- vortex-api/src/test/java/mil/army/usace/hec/vortex/math/ShifterTest.java
+++ vortex-api/src/test/java/mil/army/usace/hec/vortex/math/ShifterTest.java
@@ -1,10 +1,17 @@
 package mil.army.usace.hec.vortex.math;
 
+import hec.heclib.dss.DssDataType;
+import hec.heclib.grid.GridData;
+import hec.heclib.grid.GridInfo;
+import hec.heclib.grid.GriddedData;
 import mil.army.usace.hec.vortex.io.DataReader;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Objects;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,6 +38,45 @@ class ShifterTest {
         Set<String> destinationVariables = DataReader.getVariables(destination);
 
         assertEquals(sourceVariables.size(), destinationVariables.size());
+    }
+
+    @Test
+    void UaSweDataTypeInEqualsDataTypeOut(){
+        URL inUrl = Objects.requireNonNull(getClass().getResource(
+                "/shifter/ua_swe_in.dss"));
+
+        String in = new File(inUrl.getFile()).toString();
+
+        Set<String> variables = DataReader.getVariables(in);
+
+        URL outUrl = Objects.requireNonNull(getClass().getResource(
+                "/shifter/ua_swe_out.dss"));
+
+        String out = new File(outUrl.getFile()).toString();
+
+        Shifter shifter = Shifter.builder()
+                .shift(Duration.ofHours(-6))
+                .pathToFile(in)
+                .grids(variables)
+                .destination(out)
+                .build();
+
+        shifter.shift();
+
+        int[] status = new int[1];
+        GriddedData griddedData = new GriddedData();
+        griddedData.setDSSFileName(out);
+        griddedData.setPathname("///SWE/29FEB2020:1800/01MAR2020:1800//");
+        GridData gridData = new GridData();
+        griddedData.retrieveGriddedData(true, gridData, status);
+        if (status[0] < 0) {
+            Assertions.fail();
+        }
+
+        GridInfo gridInfo = gridData.getGridInfo();
+        Assertions.assertEquals(gridInfo.getDataType(), DssDataType.PER_AVER.value());
+
+        griddedData.done();
     }
 
 }
