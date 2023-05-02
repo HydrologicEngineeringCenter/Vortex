1d3f8567204713fb4948eade00f7b5ba17be5316
Nick Van <nghiem.t.van@usace.army.mil>
2023-05-02 12:22:45 -0500

    Update BatchImporter::build. Added 'isConcurrentWritable'

--- vortex-api/src/main/java/mil/army/usace/hec/vortex/io/BatchImporter.java
+++ vortex-api/src/main/java/mil/army/usace/hec/vortex/io/BatchImporter.java
@@ -21,7 +21,7 @@ public abstract class BatchImporter {
     private final PropertyChangeSupport support;
     private final AtomicInteger doneCount;
 
-    BatchImporter(Builder builder){
+    BatchImporter(Builder builder) {
         this.inFiles = builder.inFiles;
         this.variables = builder.variables;
         this.destination = builder.destination;
@@ -38,73 +38,69 @@ public abstract class BatchImporter {
         private final Map<String, String> geoOptions = new HashMap<>();
         private final Map<String, String> writeOptions = new HashMap<>();
 
-        public Builder inFiles(final List<String> inFiles){
+        public Builder inFiles(final List<String> inFiles) {
             this.inFiles = inFiles;
             return this;
         }
 
-        public Builder variables(final List<String> variables){
+        public Builder variables(final List<String> variables) {
             this.variables = variables;
             return this;
         }
 
-        public Builder destination(final String destination){
+        public Builder destination(final String destination) {
             this.destination = Paths.get(destination);
             return this;
         }
 
         /**
-         * @deprecated since 0.10.16, replaced by {@link #geoOptions}
-         * @param geoOptions  the geographic options
+         * @param geoOptions the geographic options
          * @return the builder
+         * @deprecated since 0.10.16, replaced by {@link #geoOptions}
          */
         @Deprecated
-        public Builder geoOptions(final Options geoOptions){
+        public Builder geoOptions(final Options geoOptions) {
             Optional.ofNullable(geoOptions).ifPresent(o -> this.geoOptions.putAll(o.getOptions()));
             return this;
         }
 
-        public Builder geoOptions(final Map<String, String> geoOptions){
+        public Builder geoOptions(final Map<String, String> geoOptions) {
             this.geoOptions.putAll(geoOptions);
             return this;
         }
 
         /**
-         * @deprecated since 0.10.16, replaced by {@link #writeOptions}
-         * @param writeOptions  the file write options
+         * @param writeOptions the file write options
          * @return the builder
+         * @deprecated since 0.10.16, replaced by {@link #writeOptions}
          */
         @Deprecated
-        public Builder writeOptions(final Options writeOptions){
+        public Builder writeOptions(final Options writeOptions) {
             Optional.ofNullable(writeOptions).ifPresent(o -> this.writeOptions.putAll(o.getOptions()));
             return this;
         }
 
-        public Builder writeOptions(Map<String, String> writeOptions){
+        public Builder writeOptions(Map<String, String> writeOptions) {
             this.writeOptions.putAll(writeOptions);
             return this;
         }
 
-        public BatchImporter build(){
-            if (destination == null){
-                throw new IllegalStateException("Invalid destination.");
-            }
+        private boolean isConcurrentWritable() {
+            PathMatcher concurrentMatcher = FileSystems.getDefault().getPathMatcher("glob:*.{dss,asc,tiff}");
+            return concurrentMatcher.matches(destination);
+        }
 
-            PathMatcher dssMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.dss");
-            if (dssMatcher.matches(destination)) {
-                return new ConcurrentBatchImporter(this);
-            }
+        public BatchImporter build() {
+            if (destination == null) throw new IllegalStateException("Invalid destination.");
 
-            PathMatcher ncMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.nc");
-            if (ncMatcher.matches(destination)) {
-                return new SerialBatchImporter(this);
-            }
-
-            throw new IllegalStateException("Invalid destination: " + destination);
+            if (isConcurrentWritable()) return new ConcurrentBatchImporter(this);
+            else return new SerialBatchImporter(this);
         }
     }
 
-    public static Builder builder() {return new Builder();}
+    public static Builder builder() {
+        return new Builder();
+    }
 
     public abstract void process();
 
@@ -137,7 +133,7 @@ public abstract class BatchImporter {
 
     PropertyChangeListener writeProgressListener(int totalCount) {
         return evt -> {
-            if(evt.getPropertyName().equals("complete")) {
+            if (evt.getPropertyName().equals("complete")) {
                 int newValue = (int) (((float) doneCount.incrementAndGet() / totalCount) * 100);
                 support.firePropertyChange("progress", null, newValue);
             }
