918d2f2d1610bb56f36866d325393aac22736b3a
Nick Van <nghiem.t.van@usace.army.mil>
2023-06-08 12:15:20 -0500

    Add double quotes to classpath for Vortex UI Windows run scripts

--- vortex-ui/package/windows/image-exporter.bat
+++ vortex-ui/package/windows/image-exporter.bat
@@ -1,4 +1,4 @@
 SET "PATH=.\gdal;.\netcdf;%PATH%"
 SET "GDAL_DATA=.\gdal\gdal-data"
 SET "PROJ_LIB=.\gdal\projlib"
-"..\jre\bin\java.exe" -Djava.library.path=".;.\gdal" --add-opens=java.desktop/sun.awt.shell=ALL-UNNAMED -cp ..\lib\* mil.army.usace.hec.vortex.ui.VortexUi -image-exporter
+"..\jre\bin\java.exe" -Djava.library.path=".;.\gdal" --add-opens=java.desktop/sun.awt.shell=ALL-UNNAMED -cp "..\lib\*" mil.army.usace.hec.vortex.ui.VortexUi -image-exporter
