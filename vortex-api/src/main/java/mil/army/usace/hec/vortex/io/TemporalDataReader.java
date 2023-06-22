9023c5a7b8c4ed874f46d17dbe61eff9283d321a
Nick Van <nghiem.t.van@usace.army.mil>
2023-06-22 12:08:16 -0500

    Clean up a bit - correct results so far. Need to improve run time.

--- vortex-api/src/main/java/mil/army/usace/hec/vortex/io/TemporalDataReader.java
+++ vortex-api/src/main/java/mil/army/usace/hec/vortex/io/TemporalDataReader.java
@@ -8,7 +8,6 @@ import mil.army.usace.hec.vortex.VortexDataType;
 import mil.army.usace.hec.vortex.VortexGrid;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -51,6 +50,110 @@ public class TemporalDataReader {
         }
     }
 
+    public VortexGrid read(ZonedDateTime startTime, ZonedDateTime endTime) {
+        VortexDataType dataType = reader.getType();
+        return read(dataType, startTime, endTime);
+    }
+
+    public VortexGrid read(VortexDataType dataType, ZonedDateTime startTime, ZonedDateTime endTime) {
+        return switch (dataType) {
+            case ACCUMULATION -> readAccumulationData(startTime, endTime);
+            case AVERAGE -> readAverageData(startTime, endTime);
+            case INSTANTANEOUS -> readInstantaneousData(startTime, endTime);
+            default -> null;
+        };
+    }
+
+    private VortexGrid readAccumulationData(ZonedDateTime startTime, ZonedDateTime endTime) {
+        List<VortexGrid> overlappedGrids = getOverlappedIntervalGrids(startTime, endTime);
+        if (overlappedGrids.isEmpty()) return null;
+        printGrids(overlappedGrids, startTime, endTime);
+
+        VortexGrid firstGrid = overlappedGrids.get(0);
+        float[] accumulationData = new float[firstGrid.data().length];
+
+        for (VortexGrid grid : overlappedGrids) {
+            double fraction = calculateFraction(grid, startTime, endTime);
+            float[] gridData = grid.data();
+            IntStream.range(0, accumulationData.length).forEach(i -> accumulationData[i] += fraction * gridData[i]);
+        }
+
+        return buildGrid(firstGrid, startTime, endTime, accumulationData);
+    }
+
+    private VortexGrid readAverageData(ZonedDateTime startTime, ZonedDateTime endTime) {
+        List<VortexGrid> overlappedGrids = getOverlappedIntervalGrids(startTime, endTime);
+        if (overlappedGrids.isEmpty()) return null;
+
+        VortexGrid firstGrid = overlappedGrids.get(0);
+        float[] averageData = new float[firstGrid.data().length];
+
+        for (VortexGrid grid : overlappedGrids) {
+            float[] gridData = grid.data();
+            IntStream.range(0, averageData.length).forEach(i -> averageData[i] += gridData[i]);
+        }
+
+        IntStream.range(0, averageData.length).forEach(i -> averageData[i] /= overlappedGrids.size());
+
+        return buildGrid(firstGrid, startTime, endTime, averageData);
+    }
+
+    private VortexGrid readInstantaneousData(ZonedDateTime startTime, ZonedDateTime endTime) {
+        if (startTime.isEqual(endTime))
+            return readInstantaneousData(startTime);
+        // Instantaneous Data for Period
+        return readPeriodInstantData(startTime, endTime);
+    }
+
+    private VortexGrid readPeriodInstantData(ZonedDateTime startTime, ZonedDateTime endTime) {
+        long start = startTime.toEpochSecond();
+        long end = endTime.toEpochSecond();
+
+        List<VortexGrid> overlappedGrids = instantDataTree.subMap(start, true, end, true)
+                .values()
+                .stream()
+                .toList();
+        if (overlappedGrids.isEmpty()) return null;
+        printGrids(overlappedGrids, startTime, endTime);
+
+        float[][] dataArrays = overlappedGrids.stream()
+                .map(VortexGrid::data)
+                .toArray(float[][]::new);
+
+        VortexGrid representativeGrid = overlappedGrids.get(0);
+        float[] weightedAverageData = calculateWeightedAverage(dataArrays);
+
+        return buildGrid(representativeGrid, startTime, endTime, weightedAverageData);
+    }
+
+    private VortexGrid readInstantaneousData(ZonedDateTime time) {
+        List<VortexGrid> gridList = getOverlappedInstantGrids(time);
+
+        if (gridList.isEmpty()) return null;
+        if (gridList.size() == 1) return gridList.get(0); // Found grid that matches the exact time
+
+        // Calculating instantaneous data using dataLeft and dataRight
+        VortexGrid nearestBefore = gridList.get(0);
+        VortexGrid nearestAfter = gridList.get(1);
+
+        long nearestBeforeEpoch = nearestBefore.startTime().toEpochSecond();
+        long nearestAfterEpoch = nearestAfter.startTime().toEpochSecond();
+        long targetTimeEpoch = time.toEpochSecond();
+
+        double ratio = (double) (targetTimeEpoch - nearestBeforeEpoch) / (nearestAfterEpoch - nearestBeforeEpoch);
+
+        float[] beforeData = nearestBefore.data();
+        float[] afterData = nearestAfter.data();
+        float[] resultData = new float[beforeData.length];
+
+        for (int i = 0; i < beforeData.length; i++) {
+            resultData[i] = (float) (beforeData[i] + (afterData[i] - beforeData[i]) * ratio);
+        }
+
+        return buildGrid(nearestBefore, time, time, resultData);
+    }
+
+    /* Helpers */
     private List<VortexGrid> getOverlappedIntervalGrids(ZonedDateTime startTime, ZonedDateTime endTime) {
         long queryStart = startTime.toEpochSecond();
         long queryEnd = endTime.toEpochSecond();
@@ -79,142 +182,9 @@ public class TemporalDataReader {
         return List.of(floorIndex, ceilingIndex);
     }
 
-    /**
-     * Reads a VortexGrid from the data source, with the type of data
-     * determined by the VortexDataType. The VortexGrid will cover the time interval
-     * from startTime to endTime.
-     *
-     * <ul>
-     *   <li>If the data type is ACCUMULATION, it reads accumulation data.
-     *   <li>If the data type is AVERAGE, it reads average data.
-     *   <li>If the data type is INSTANTANEOUS, it reads instantaneous data for the start time only.
-     * </ul>
-     *
-     * If the data type is not recognized, or if the data for the specified time
-     * interval is not found, the method returns null.
-     *
-     * @param startTime The start of the time interval for which to read data.
-     * @param endTime The end of the time interval for which to read data.
-     * @return A VortexGrid covering the specified time interval, or null if the
-     * data type is not recognized or data is not found.
-     */
-    public VortexGrid read(ZonedDateTime startTime, ZonedDateTime endTime) {
-        VortexDataType dataType = reader.getType();
-        return read(dataType, startTime, endTime);
-    }
-
-    public VortexGrid read(VortexDataType dataType, ZonedDateTime startTime, ZonedDateTime endTime) {
-        VortexGrid matchingGridInBuffer = findMatchingGridInBuffer(startTime, endTime);
-        if (matchingGridInBuffer != null) return matchingGridInBuffer;
-
-        return switch (dataType) {
-            case ACCUMULATION -> readAccumulationData(startTime, endTime);
-            case AVERAGE -> readAverageData(startTime, endTime);
-            case INSTANTANEOUS -> readInstantaneousData(startTime, endTime);
-            default -> null;
-        };
-    }
-
-    /**
-     * Mathematically, the accumulation data (for each point in the grid) is calculated as follows:
-     * <pre>
-     *     accumulationData[i] += fraction * gridData[i]
-     * </pre>
-     * Where:
-     * <ul>
-     *   <li>'i' is the index of the point in the grid.
-     *   <li>fraction is the time fraction that the current grid contributes.
-     *   <li>gridData[i] is the data point at index i in the current grid.
-     * </ul>
-     * @param startTime the start of the time period
-     * @param endTime the end of the time period
-     * @return a VortexGrid that represents the accumulated data of all overlapped grids
-     *         for the given time period, or null if no overlapped grids were found.
-     */
-    private VortexGrid readAccumulationData(ZonedDateTime startTime, ZonedDateTime endTime) {
-        List<VortexGrid> overlappedGrids = getOverlappedIntervalGrids(startTime, endTime);
-        if (overlappedGrids.isEmpty()) return null;
-        printGrids(overlappedGrids, startTime, endTime);
-
-        VortexGrid firstGrid = overlappedGrids.get(0);
-        float[] accumulationData = new float[firstGrid.data().length];
-
-        for (VortexGrid grid : overlappedGrids) {
-            double fraction = calculateFraction(grid, startTime, endTime);
-            float[] gridData = grid.data();
-            IntStream.range(0, accumulationData.length).forEach(i -> accumulationData[i] += fraction * gridData[i]);
-        }
-
-        return buildGrid(firstGrid, startTime, endTime, accumulationData);
-    }
-
-    /**
-     * The function calculates the average data by summing up the values of overlapped grids and dividing the sum by the
-     * number of overlapped grids. Mathematically, the calculation for each location `i` can be represented as:
-     * <pre>
-     *     averageData[i] = (grid1Data[i] + grid2Data[i] + ... + gridNData[i]) / N
-     * </pre>
-     * Where:
-     * <ul>
-     *   <li>`averageData[i]` represents the computed average data for location `i`
-     *   <li>`grid1Data[i]...gridNData[i]` are the data values of the overlapped grids at location `i`
-     *   <li>`N` is the number of overlapped grids.
-     * </ul>
-     * @param startTime The start of the time interval for which to read and compute the average data.
-     * @param endTime The end of the time interval for which to read and compute the average data.
-     * @return A VortexGrid object representing the average data for the specified time interval, or null if no overlapped grids are found.
-     */
-    private VortexGrid readAverageData(ZonedDateTime startTime, ZonedDateTime endTime) {
-        List<VortexGrid> overlappedGrids = getOverlappedIntervalGrids(startTime, endTime);
-        if (overlappedGrids.isEmpty()) return null;
-
-        VortexGrid firstGrid = overlappedGrids.get(0);
-        float[] averageData = new float[firstGrid.data().length];
-
-        for (VortexGrid grid : overlappedGrids) {
-            float[] gridData = grid.data();
-            IntStream.range(0, averageData.length).forEach(i -> averageData[i] += gridData[i]);
-        }
-
-        IntStream.range(0, averageData.length).forEach(i -> averageData[i] /= overlappedGrids.size());
-
-        return buildGrid(firstGrid, startTime, endTime, averageData);
-    }
-
-    private VortexGrid readInstantaneousData(ZonedDateTime startTime, ZonedDateTime endTime) {
-        if (startTime.isEqual(endTime))
-            return readInstantaneousData(startTime);
-        // Instantaneous Data for Period
-        return readPeriodInstantData(startTime, endTime);
-//        ZonedDateTime midTime = calculateMidTime(startTime, endTime);
-//        VortexGrid grid = readInstantaneousData(midTime);
-//        return grid != null ? buildGrid(grid, startTime, endTime, grid.data()) : null;
-    }
-
-    private VortexGrid readPeriodInstantData(ZonedDateTime startTime, ZonedDateTime endTime) {
-        long start = startTime.toEpochSecond();
-        long end = endTime.toEpochSecond();
-
-        List<VortexGrid> overlappedGrids = instantDataTree.subMap(start, true, end, true)
-                .values()
-                .stream()
-                .toList();
-        if (overlappedGrids.isEmpty()) return null;
-        printGrids(overlappedGrids, startTime, endTime);
-
-        float[][] dataArrays = overlappedGrids.stream()
-                .map(VortexGrid::data)
-                .toArray(float[][]::new);
-
-        VortexGrid representativeGrid = overlappedGrids.get(0);
-        float[] weightedAverageData = calculateWeightedAverage(dataArrays);
-
-        return buildGrid(representativeGrid, startTime, endTime, weightedAverageData);
-    }
-
     private float[] calculateWeightedAverage(float[]... dataArrays) {
         int numDataArrays = dataArrays.length;
-        if (numDataArrays == 0) return null;
+        if (numDataArrays == 0) return new float[0];
 
         // NOTE: Weighted Average: first and last data weight is 0.5
         int numData = dataArrays[0].length;
@@ -232,52 +202,6 @@ public class TemporalDataReader {
         return weightedAverage;
     }
 
-    /**
-     * The function calculates the instantaneous data by
-     * <pre>
-     *     ratio = (targetTimeEpoch - nearestBeforeEpoch) / (nearestAfterEpoch - nearestBeforeEpoch);
-     *     resultData[i] = beforeData[i] + (afterData[i] - beforeData[i]) * ratio
-     * </pre>
-     * Where:
-     * <ul>
-     *   <li>`ratio` is a measure of how far time is between nearestBeforeTime and nearestAfterTime.
-     *   <li>`grid1Data[i]...gridNData[i]` are the data values of the overlapped grids at location `i`
-     *   <li>`N` is the number of overlapped grids.
-     * </ul>
-     * @param time The instantaneous time which to read and compute the instantaneous data.
-     * @return A VortexGrid object representing the instantaneous data for the specified time, or null if no data is found.
-     */
-    private VortexGrid readInstantaneousData(ZonedDateTime time) {
-        List<VortexGrid> gridList = getOverlappedInstantGrids(time);
-
-        if (gridList.isEmpty()) return null;
-        if (gridList.size() == 1) return gridList.get(0); // Found grid that matches the exact time
-
-        // Calculating instantaneous data using dataLeft and dataRight
-        VortexGrid nearestBefore = gridList.get(0);
-        VortexGrid nearestAfter = gridList.get(1);
-
-        long nearestBeforeEpoch = nearestBefore.startTime().toEpochSecond();
-        long nearestAfterEpoch = nearestAfter.startTime().toEpochSecond();
-        long targetTimeEpoch = time.toEpochSecond();
-
-        double ratio = (double) (targetTimeEpoch - nearestBeforeEpoch) / (nearestAfterEpoch - nearestBeforeEpoch);
-
-        float[] beforeData = nearestBefore.data();
-        float[] afterData = nearestAfter.data();
-        float[] resultData = new float[beforeData.length];
-
-        for (int i = 0; i < beforeData.length; i++) {
-            float beforeValue = beforeData[i];
-            float afterValue = afterData[i];
-            float diff = afterValue - beforeValue;
-            resultData[i] = (float) (beforeData[i] + (afterData[i] - beforeData[i]) * ratio);
-        }
-
-        return buildGrid(nearestBefore, time, time, resultData);
-    }
-
-    /* Helpers */
     private VortexGrid buildGrid(VortexGrid grid, ZonedDateTime startTime, ZonedDateTime endTime, float[] data) {
         return VortexGrid.builder()
                 .dx(grid.dx())
@@ -301,38 +225,10 @@ public class TemporalDataReader {
                 .build();
     }
 
-    /**
-     * Calculates the proportion of a VortexGrid's period that overlaps with a given time range.
-     * <p>
-     * The function first determines the overlap of the VortexGrid's period with the target time range.
-     * Then, it calculates the proportion that this overlap represents.
-     *
-     * @param grid The VortexGrid object.
-     * @param startTime The start of the target time range.
-     * @param endTime The end of the target time range.
-     * @return The proportion of the VortexGrid's period that overlaps with the target time range.
-     */
     private double calculateFraction(VortexGrid grid, ZonedDateTime startTime, ZonedDateTime endTime) {
         double overlapStart = Math.max(startTime.toEpochSecond(), grid.startTime().toEpochSecond());
         double overlapEnd = Math.min(endTime.toEpochSecond(), grid.endTime().toEpochSecond());
         return (overlapEnd - overlapStart) / (grid.endTime().toEpochSecond() - grid.startTime().toEpochSecond());
-    }
-
-    private ZonedDateTime calculateMidTime(ZonedDateTime startTime, ZonedDateTime endTime) {
-        long startSeconds = startTime.toEpochSecond();
-        long endSeconds = endTime.toEpochSecond();
-        long midSeconds = (startSeconds + endSeconds) / 2;
-        return ZonedDateTime.ofInstant(Instant.ofEpochSecond(midSeconds), startTime.getZone());
-    }
-
-    private VortexGrid findMatchingGridInBuffer(ZonedDateTime startTime, ZonedDateTime endTime) {
-        for (VortexGrid grid : reader.getBuffer()) {
-            boolean sameStartTime = grid.startTime().isEqual(startTime);
-            boolean sameEndTime = grid.endTime().isEqual(endTime);
-            if (sameStartTime && sameEndTime) return grid;
-        }
-
-        return null;
     }
 
     private void printGrids(Collection<VortexGrid> grids, ZonedDateTime startTime, ZonedDateTime endTime) {
