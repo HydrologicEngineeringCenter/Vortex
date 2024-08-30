465b4e90ef9147bdd41733bc16086d7a15a1b35b
Nick Van <nghiem.t.van@usace.army.mil>
2024-08-30 16:22:43 -0500

    Increase memory threshold to 0.8

--- vortex-api/src/main/java/mil/army/usace/hec/vortex/io/buffer/MemoryManager.java
+++ vortex-api/src/main/java/mil/army/usace/hec/vortex/io/buffer/MemoryManager.java
@@ -2,7 +2,7 @@ package mil.army.usace.hec.vortex.io.buffer;
 
 public final class MemoryManager {
     private static final Runtime runtime = Runtime.getRuntime();
-    private static final double DEFAULT_MEMORY_THRESHOLD = 0.6;
+    private static final double DEFAULT_MEMORY_THRESHOLD = 0.8;
 
     private MemoryManager() {
         // Utility Class
