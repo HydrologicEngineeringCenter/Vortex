71845e5ca07f394e889611ceed97d0a9d9d9ce0b
Tom Brauer <thomas.brauer@usace.army.mil>
2026-04-01 14:27:57 -0700

    Add ShifterWizard back for backwards compatibility
    ShifterWizard was removed when Shifter was renamed to TimeShifter. Re-add it as a deprecated subclass of TimeShifterWizard so that existing code referencing the old name continues to compile.

---
+++ vortex-ui/src/main/java/mil/army/usace/hec/vortex/ui/ShifterWizard.java
@@ -0,0 +1,13 @@
+package mil.army.usace.hec.vortex.ui;
+
+import java.awt.*;
+
+/**
+ * @deprecated since 0.13.0 replaced by {@link TimeShifterWizard}
+ */
+@Deprecated
+public class ShifterWizard extends TimeShifterWizard {
+    public ShifterWizard(Frame frame) {
+        super(frame);
+    }
+}
